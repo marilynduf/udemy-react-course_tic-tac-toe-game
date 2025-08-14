cc279ed188a1d40bbdb887cb80abccab6fe72272
M <marilynduf@gmail.com>
2025-08-14 19:00:23 -0400

    Toggle button Edit/Save player name

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    let buttonCaption = isEditing ? "Save" : "Edit";
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+    let playerName = <span className="player-name">{name}</span>;
+    if (isEditing) {
+        playerName = <input type="text" required value={name} />;
+    }
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {playerName}
                 <span className="symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{buttonCaption}</button>
         </li>
     );
 }
