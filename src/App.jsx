c8ecc91e8c217d57947152263ac72daa7f2d914f
M <marilynduf@gmail.com>
2025-08-14 18:26:48 -0400

    Ajout Player composant

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Player from "./Components/Player";
 function App() {
-    return <p>Comming soon</p>;
+    console.log("APP componement rendered");
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player name="Player 1" symbol="X"></Player>
+                    <Player name="Player 2" symbol="O"></Player>
+                </ol>
+                PLAYERS GAME BOARD
+            </div>
+            LOG
+        </main>
+    );
 }
 
 export default App;
---
+++ src/Components/Player.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+    return (
+        <li>
+            <span className="player">
+                <span className="player-name">{name}</span>
+                <span className="symbol">{symbol}</span>
+            </span>
+            <button>Edit</button>
+        </li>
+    );
+}
