633c98ecf01616989998657e54f386612b5277ce
M <marilynduf@gmail.com>
2025-08-15 13:40:42 -0400

    Ajout fonction edit/save player name

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
     let buttonCaption = isEditing ? "Save" : "Edit";
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
     }
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        console.log(event);
+        setPlayerName(() => event.target.value);
+    }
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
+        editablePlayerName = (
+            <input
+                type="text"
+                required
+                value={playerName}
+                onChange={handleChange}
+            />
+        );
     }
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{buttonCaption}</button>
