3dd75411fec77185524fcc9431d20dc6da240394
M <marilynduf@gmail.com>
2025-08-21 16:36:40 -0400

    Ajout condition disabled sur carre deja coche par joueur

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -25,6 +25,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
                                     onClick={() =>
                                         onSelectSquare(rowIndex, colIndex)
                                     }
+                                    disabled={playerSymbol !== null}
                                 >
                                     {playerSymbol}
                                 </button>
